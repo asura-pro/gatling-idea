d5bb2b12a231061b686e4c8e39d1b098b369dc00
zhengshaodong <ngxcorpio@gmail.com>
2019-05-19 23:55:38 +0800

    Add basic files

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,5 +4,5 @@ object Dependencies {
   lazy val gatling = Seq(
     "io.gatling.highcharts" % "gatling-charts-highcharts",
     "io.gatling" % "gatling-test-framework"
-  ).map(_ % "3.1.2" % Test)
+  ).map(_ % "3.1.2")
 }
